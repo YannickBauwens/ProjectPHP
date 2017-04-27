136f3e5220befc6713f57d71a788352581d9bfae
kennymn <kenny_malangombe@hotmail.com>
2017-04-27 21:26:08 +0200

    update profile page

--- ProjectPHP/profile.php
+++ ProjectPHP/profile.php
@@ -1,18 +1,9 @@
 <?php
 
     include_once("includes/no-session.inc.php");
+    include_once("includes/nav.inc.php");
 
-    spl_autoload_register(function ($class) {
-        include_once 'classes/' . $class . '.class.php';
-    });
 
-    $user = new User();
-    $user->Id = $_GET['id'];
-    if ($user->checkIfUserExists()) {
-        $user->getDataFromDatabase();
-    } else {
-        header("Location: 404.php");
-    }
 
 ?>
 <!DOCTYPE html>
@@ -20,49 +11,31 @@
 
     <head>
         <meta charset="UTF-8">
-        <title>My profile</title>
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title><?php echo $_SESSION['email']; ?></title>
         <link rel="stylesheet" href="css/reset.css">
-        <link rel="stylesheet" href="css/style.css">
+        <!--<link rel="stylesheet" href="css/style.css">-->
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     </head>
 
     <body>
 
-       <div class="alles">
-            <div class="profile">
-                <div class="About">
+    <div class="profileHeader">
+        <img class="avatar" src="images/posts/5_1493295164.jpeg" alt="">
+        <h2><?php echo $_SESSION['email']; ?></h2>
 
-                   <div>
-                        <h1 class="h1Profile"><?php echo $user->firstname . " " . $user->lastname; ?></h1>
-                        <h2 class="h3Profile"><?php echo $user->username; ?></h2>
-                        <form action="settings.php" class="formProf">
-                            <input type="submit" value="Edit profile">
-                        </form>
-                    </div>
+    </div>
 
-                    <img class="profile-picture" src="<?php echo $user->Avatar; ?>">
-                    <br class=“clearfix”>
 
-                </div>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-                <div class="profile-information">
-                    <div class="profile-information-item">
-                        <span>Photos</span>
-                        <span class="amount"><?php echo $user->PhotosNr; ?></span>
-                    </div>
-                    <div class="profile-information-item">
-                        <span>Followers</span>
-                        <span class="amount"><?php echo $user->FollowersNr; ?></span>
-                    </div>
-                    <div class="profile-information-item">
-                        <span>Following</span>
-                        <span class="amount"><?php echo $user->FollowingNr; ?></span>
-                    </div>
-                </div>
-                <div class="overlay"></div>
-            </div>
-        
-        </div>
-        
-</body>
+    </body>
 
 </html>
